5cd082a2cfa5bc05e14addc75620338906b0dc56
SrinivasKatherasala12 <katherasala.srinivas34@gmail.com>
2025-04-07 04:52:49 +0530

    Update Restaurants.java

--- src/Restaurants/Restaurants.java
+++ src/Restaurants/Restaurants.java
@@ -33,37 +33,39 @@ public class Restaurants {
 			WebElement searchrestaurants = driver
 					.findElement(By.xpath("//input[@placeholder=\"Search for restaurants...\"]"));
 			searchrestaurants.sendKeys("Swaniawski and Sons");
-			Thread.sleep(5000);
-			WebElement clickOnSwaniawskiRestaurants = driver.findElement(By.xpath("//h2[contains(text(),\"Swaniawski and Sons\")]"));
+
+			//Thread.sleep() is used to pause the execution temporarily, which helps in observing the flow of the web page more easily. However, it is generally recommended to use one of the three types of waits available in Selenium for better efficiency and reliability: Implicit Wait, Explicit Wait, and Fluent Wait.
+			Thread.sleep(3000);
+			WebElement clickOnSwaniawskiRestaurant = driver.findElement(By.xpath("//h2[contains(text(),\"Swaniawski and Sons\")]"));
 			clickOnSwaniawskiRestaurants.click();
-			Thread.sleep(5000);
-			WebElement addToCart = driver.findElement(By.xpath("//button[contains(text(),'Add to Cart')][1]"));
+			Thread.sleep(3000);
+			WebElement addToCartBtn = driver.findElement(By.xpath("//button[contains(text(),'Add to Cart')][1]"));
 			addToCart.click();
-			Thread.sleep(5000);
-			WebElement ClcikAddToCartIcon = driver.findElement(By.xpath("//a[@class=\"flex items-center justify-center w-16 h-16 bg-red-600 text-white rounded-full shadow-lg hover:bg-red-700 transition-colors\"]"));
+			Thread.sleep(3000);
+			WebElement clickOnAddToCartIcon = driver.findElement(By.xpath("//a[@class=\"flex items-center justify-center w-16 h-16 bg-red-600 text-white rounded-full shadow-lg hover:bg-red-700 transition-colors\"]"));
 			ClcikAddToCartIcon.click();
-			Thread.sleep(5000);
-			WebElement clickOnProceedtoCheckout= driver.findElement(By.xpath("//button[contains(text(),'Proceed to Checkout')]"));
+			Thread.sleep(3000);
+			WebElement clickOnProceedtoCheckoutBtn= driver.findElement(By.xpath("//button[contains(text(),'Proceed to Checkout')]"));
 			clickOnProceedtoCheckout.click();
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 			WebElement clickOnStreetAddress= driver.findElement(By.xpath("//input[@id=\"address\"]"));
 			clickOnStreetAddress.sendKeys("Sri Prasanna Anajaneya nagar");
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 			WebElement clickOnCity= driver.findElement(By.xpath("//input[@id=\"city\"]"));
 			clickOnCity.sendKeys("Devapur");
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 			WebElement clickOnZipCode= driver.findElement(By.xpath("//input[@id=\"zipCode\"]"));
 			clickOnZipCode.sendKeys("504218");
-			Thread.sleep(5000);
-			WebElement clickOnPhone= driver.findElement(By.xpath("//input[@id=\"phone\"]"));
+			Thread.sleep(3000);
+			WebElement clickOnPhoneNumber= driver.findElement(By.xpath("//input[@id=\"phone\"]"));
 			clickOnPhone.sendKeys("8374458189");
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 			WebElement clickPlaceOrder= driver.findElement(By.xpath("//button[contains(text(),'Place Order')]"));
 			clickPlaceOrder.click();
-			Thread.sleep(5000);
-			WebElement clickContinueShopping= driver.findElement(By.xpath("//a[contains(text(),'Continue Shopping')]"));
+			Thread.sleep(3000);
+			WebElement clickContinueShoppingBtn= driver.findElement(By.xpath("//a[contains(text(),'Continue Shopping')]"));
 			clickContinueShopping.click();
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		
 		} catch (Exception e) {
 		}
